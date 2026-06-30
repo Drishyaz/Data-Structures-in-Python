1604fdacd146f69b8258d1e0e44236e7df437b0f
Drishya Agarwal <106886727+Drishyaz@users.noreply.github.com>
2026-06-30 23:36:32 +0530

    Update linearSearch.py

--- Python/linearSearch.py
+++ Python/linearSearch.py
@@ -1,1 +1,20 @@
+def linear(l,item):
+    if item in l:
+        for i in l:
+            if i == item:
+                indx = l.index(item)
+                print("Item",item,"found at index",indx)
+    else:
+        print("Item not found in list")
 
+def traverse(l):
+    print("Traversing the list: ")
+    print(l[:])
+
+def main():
+  item = int(input("Enter item to search: "))
+  l = map(int,input().split())
+  linear(l,item)
+  
+if __name__ == '__main__':
+  main()
